0e19578b346c9fbfb8d8250e018a117455093965
LucasLemeCF <lucasleme09@gmail.com>
2021-01-17 17:09:36 -0300

    aula 4
    Conteudo da aula 4 do projeto

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ const dino = document.querySelector('.dino');
 
 function handKeyUp(event) {
     if (event.keyCode === 32) {
-        console.log("funcionou");
+        jump();
     }
+}
+
+function jump() {
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            //Descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                } 
+            }, 20);
+        } else {
+            //Subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 
 document.addEventListener('keyup', handKeyUp);
