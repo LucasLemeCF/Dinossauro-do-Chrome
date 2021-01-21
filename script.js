13d8ba0cfe636838fdcc4b0058879d1802430dda
LucasLemeCF <lucasleme09@gmail.com>
2021-01-20 22:09:55 -0300

    Grandes mudanças no jogo
    -Adicionado a opção de troca de dino
    -Melhorado o gameplay
    -Mudanças no design
    -Varias correções de bugs

--- script.js
+++ script.js
@@ -40,15 +40,15 @@ function jump() {
 
 function createCactus() {
     const cactus = document.createElement('div');
-    let cactusPosition = 1000;
-    let randomTime = Math.random() * 6000;
+    let cactusPosition = 950;
+    let randomTime = Math.random() * (3000 - 750) + 750;
 
     cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
+    cactus.style.left = cactusPosition + 'px';
     background.appendChild(cactus);
 
     let leftInterval = setInterval(() => {
-        if (cactusPosition < -70) {
+        if (cactusPosition < 20) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
         } else if (cactusPosition > 0 && cactusPosition < 70 && position < 70) {
@@ -56,20 +56,14 @@ function createCactus() {
             clearInterval(leftInterval);
             background.removeChild(cactus);
             stopGame = true;
-            dino.classList.remove("dino");
-            document.querySelector(".pontuacao").style.display = 'none';  
-            document.querySelector("#gameOver").style.display = 'block';  
-            document.querySelector("#gameOver").innerHTML = 'Game Over </br> Pontruação: ' + pontos; 
             gameOver();
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
         }
-    }, 20);
+    }, 30);
 
-    if (stopGame == false) {
-        setTimeout(createCactus, randomTime);
-    }
+    setTimeout(createCactus, randomTime); 
 }
 
 function pontuacao() { 
@@ -92,25 +86,52 @@ function jogar() {
 }
 
 function gameOver() {
+    document.querySelector(".pontuacao").style.display = 'none';  
+    document.querySelector("#gameOver").style.display = 'block';  
+    document.querySelector("#gameOver").innerHTML = 'Game Over </br> Pontruação: ' + pontos; 
     document.querySelector("#jogar").style.display = 'none';  
-    document.querySelector("#escolherDino").style.display = 'none';  
+    document.querySelector("#mostrarDinos").style.display = 'none';  
     document.querySelector("#sobre").style.display = 'none';  
-    document.querySelector("#reiniciar").style.display = 'block';  
-    document.querySelector(".menu").style.display = 'block';  
-    document.querySelector("#removeCactus").style.display = 'block';  
+    document.querySelector("#reiniciar").style.display = 'hidden';  
+    document.querySelector(".menu").style.display = 'flex';  
+    document.querySelector("#removeCactus").style.display = 'hidden';  
+    document.querySelector("#background").style.display = 'none';  
+    document.querySelector(".atualizar").style.display = 'block'; 
+    document.querySelector(".animation").style.display = 'block'; 
 }
 
-function escolherDino() {
+function atualizar() {
+    document.location.reload(true);
+}
 
+function mostrarDinos() {
+    document.querySelector(".menu").style.display = 'none';  
+    document.querySelector(".menu-dino").style.display = 'flex';  
+}
+
+function escolherDino(dino, id) {
+    document.querySelector(".dino").style.background = "url('sprites/dino-" + id + ".gif')";
+    document.querySelector(".dino").style.backgroundSize = "100%";  
 }
 
 function mostrarSobre() {
     document.querySelector("#jogar").style.display = 'none';  
-    document.querySelector("#escolherDino").style.display = 'none';  
+    document.querySelector("#mostrarDinos").style.display = 'none';  
     document.querySelector("#sobre").style.display = 'none';  
     document.querySelector("#linkedIn").style.display = 'block';  
     document.querySelector("#gitHub").style.display = 'block';  
-    document.querySelector("#voltar").style.display = 'block';  
+    document.querySelector(".voltar").style.display = 'block';  
+}
+
+function voltar() {
+    document.querySelector(".menu").style.display = 'flex';  
+    document.querySelector(".menu-dino").style.display = 'none';  
+    document.querySelector("#jogar").style.display = 'block';  
+    document.querySelector("#mostrarDinos").style.display = 'block';  
+    document.querySelector("#sobre").style.display = 'block';  
+    document.querySelector("#linkedIn").style.display = 'none';  
+    document.querySelector("#gitHub").style.display = 'none';  
+    document.querySelector(".voltar").style.display = 'none';  
 }
 
 document.querySelector(".pontuacao").innerHTML = "Pontuação: 0";
